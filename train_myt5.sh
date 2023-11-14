a5ac687b6279e31401d2b4fa8be57fd164fedd80
tomasz <t.limisiewicz@gmail.com>
2023-11-13 21:00:39 -0800

    Train myt5 model

---
+++ train_myt5.sh
@@ -0,0 +1,12 @@
+# Model dir to save logs, ckpts, etc. in "gs://model_dir" format.
+
+
+EXPERIMENT="myt5_small"
+MODEL_DIR="gs://${BUCKET}/models/${EXPERIMENT}"
+T5X_DIR="/home/${ACCOUNT}/t5x"  # directory where the T5X repo is cloned.
+TFDS_DATA_DIR="gs://${BUCKET}/data"
+
+python3 ${T5X_DIR}/t5x/train.py \
+  --gin_file="${T5X_DIR}/t5x/examples/pretrain_myt5_mc4.gin" \
+  --gin.MODEL_DIR="'${MODEL_DIR}'" \
+  --tfds_data_dir=${TFDS_DATA_DIR}
