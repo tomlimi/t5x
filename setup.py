c6241466aa5016a43c0ca24a18f476a9294f8b0e
tomasz <t.limisiewicz@gmail.com>
2023-11-24 00:09:59 -0800

    added import of tensorflow-text

--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ setuptools.setup(
         'seqio @ git+https://github.com/google/seqio#egg=seqio',
         'tensorflow-cpu > 2.14.0',
         'tensorstore >= 0.1.20',
+        'tensorflow-text',
         # remove this when sentencepiece_model_pb2 is re-generated in the
         # sentencepiece package.
         'protobuf==3.20.3',
