3113f42b6731d6b2935d73489bb5ce2fcfc3f19f
tomasz <t.limisiewicz@gmail.com>
2023-12-12 02:24:06 -0800

    last fixes

--- setup.py
+++ setup.py
@@ -53,8 +53,8 @@ setuptools.setup(
         'flax', #@ git+https://github.com/google/flax#egg=flax',
         'fiddle >= 0.2.5',
         'gin-config',
-        f'jax >= {_jax_version}',
-        f'jaxlib >= {_jaxlib_version}',
+        #f'jax >= {_jax_version}',
+        f'jaxlib -f https://storage.googleapis.com/jax-releases/jax_releases.html',
         (
             'jestimator @'
             ' git+https://github.com/google-research/jestimator#egg=jestimator'
@@ -82,7 +82,7 @@ setuptools.setup(
         ],
         'test': ['pytest', 't5'],
         # Cloud TPU requirements.
-        'tpu': [f'jax[tpu] >= {_jax_version}'],
+        'tpu': [f'jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html'],
         'gpu': [
             'ipdb==0.13.9',
             'fasttext==0.9.2',
